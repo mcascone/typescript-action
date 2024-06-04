b5fd07d1f8dcfe2ef88b7b482dfd8f9808fc82c9
Max Cascone <maxcascone@gmail.com>
2024-06-04 19:22:20 +0000

    fix test

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -48,7 +48,10 @@ describe('action', () => {
     expect(runMock).toHaveReturned()
 
     // Verify that all of the core library functions were called correctly
-    expect(debugMock).toHaveBeenNthCalledWith(1, 'Waiting 500 milliseconds ...')
+    expect(debugMock).toHaveBeenNthCalledWith(
+      1,
+      'Waiting for 500 milliseconds ...'
+    )
     expect(debugMock).toHaveBeenNthCalledWith(
       2,
       expect.stringMatching(timeRegex)
